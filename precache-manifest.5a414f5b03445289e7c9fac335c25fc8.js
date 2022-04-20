1547a4fb1bcb8954d502b064eedd685fedfea741
V.S.Naaviinesh <v.s.naaviinesh@outlook.com>
2022-04-20 22:13:04 +0800

    Updates

---
+++ precache-manifest.5a414f5b03445289e7c9fac335c25fc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fe28ed3ed0e94660a08",
+    "url": "/space-api-news/static/css/main.32e5e45b.chunk.css"
+  },
+  {
+    "revision": "0fe28ed3ed0e94660a08",
+    "url": "/space-api-news/static/js/main.0fe28ed3.chunk.js"
+  },
+  {
+    "revision": "dbf16a7a1f5d12f53526",
+    "url": "/space-api-news/static/js/1.dbf16a7a.chunk.js"
+  },
+  {
+    "revision": "bb5f21b3670b3090b3c7",
+    "url": "/space-api-news/static/js/2.bb5f21b3.chunk.js"
+  },
+  {
+    "revision": "948d9eaa17f6de7d2cb0",
+    "url": "/space-api-news/static/js/runtime~main.948d9eaa.js"
+  },
+  {
+    "revision": "c264308e5a8d5e7c4b06039365c5131a",
+    "url": "/space-api-news/index.html"
+  }
+];
